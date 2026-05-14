29e87316688fa2ddfed5aae3d89fc4185fc6f0bd
Mohsen Samiee <Mo.Samiee@Outlook.com>
2026-05-14 13:11:12 -0400

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,15 @@
+import userGenres from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { genres, error, isLoading } = userGenres();
+
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
