ea609ca7ee4e6d19cd9c149bf14e727c26834f41
Mohsen Samiee <Mo.Samiee@Outlook.com>
2026-05-15 11:18:33 -0400

    Handle game with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from "../assets/placeHolderImage.svg";
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   let index = url.indexOf(target);
   if (index === -1) {
