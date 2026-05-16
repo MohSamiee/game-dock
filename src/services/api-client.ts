20bacaca52a1181f024a87664b6e7206210e52a8
Mohsen Samiee <Mo.Samiee@Outlook.com>
2026-05-16 01:09:15 -0400

    Fix: replace hardcoded API key with environment variable

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "9da65e03a36a4c7cab8509df871a372e",
+    key: import.meta.env.VITE_RAWG_KEY,
   },
 });
